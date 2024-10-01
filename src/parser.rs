8ec1f0f16ce3088757e11bf88c4ba919e0c72ec7
Austin Richards <austinjrichards@outlook.com>
2024-10-01 07:06:16 -0600

    Fixed parsing binary expressions, had incorrect lexemes specified

--- src/parser.rs
+++ src/parser.rs
@@ -8,7 +8,6 @@ use std::iter::Peekable;
 // In order of lowest to highest precedence:
 // Comma Expressions
 // Ternary Expressions
-// Expression
 // Comparison
 // Addition
 // Multiplication
@@ -28,16 +27,15 @@ impl Parser<'_> {
     }
 
     fn expression(&mut self) -> Expr {
-        println!("parse: expression");
         self.unary_reverse()
     }
 
     fn unary_reverse(&mut self) -> Expr {
-        println!("parse: unary reverse");
         // used for comma expressions
         let expr = self.ternary();
         let unary_reverse_fields = [TokenType::Comma];
         if let Some(_value) = self.tokens.next_if(|x| unary_reverse_fields.contains(&x.token_type)) {
+            println!("parse: unary reverse");
             self.current += 1;
             let operator = self.token_vec[self.current].clone();
             return Expr::UnaryReverse{left: Box::new(expr), operator};
@@ -46,10 +44,10 @@ impl Parser<'_> {
     }
 
     fn ternary(&mut self) -> Expr {
-        println!("parse: ternary");
         let expr = self.equality();
         let ternary_fields = [TokenType::Question];
         if let Some(_value) = self.tokens.next_if(|x| ternary_fields.contains(&x.token_type)) {
+            println!("parse: ternary");
             self.current += 1;
             let if_true = self.equality();
             let middle_ternary_fields = [TokenType::Colon];
@@ -64,11 +62,11 @@ impl Parser<'_> {
 
 
     fn equality(&mut self) -> Expr {
-        println!("parse: equality");
         let mut expr = self.comparison();
 
         let equality_fields = [TokenType::EqualEqual, TokenType::BangEqual];
         while let Some(_value) = self.tokens.next_if(|x| equality_fields.contains(&x.token_type)) {
+            println!("parse: equality");
             self.current += 1;
             let operator = self.previous_token();
             let right = self.comparison();
@@ -78,10 +76,10 @@ impl Parser<'_> {
     }
 
     fn comparison(&mut self) -> Expr {
-        println!("parse: comparison");
         let mut expr = self.term();
         let comparison_fields = [TokenType::LessEqual, TokenType::Less, TokenType::Greater, TokenType::GreaterEqual];
         while let Some(_value) = self.tokens.next_if(|x| comparison_fields.contains(&x.token_type)) {
+            println!("parse: comparison");
             self.current += 1;
             let operator = self.previous_token();
             let right = self.term();
@@ -91,10 +89,10 @@ impl Parser<'_> {
     }
 
     fn term(&mut self) -> Expr {
-        println!("parse: term");
         let mut expr = self.factor();
-        let term_fields = [TokenType::LessEqual, TokenType::Less, TokenType::Greater, TokenType::GreaterEqual];
+        let term_fields = [TokenType::Minus, TokenType::Plus];
         while let Some(_value) = self.tokens.next_if(|x| term_fields.contains(&x.token_type)) {
+            println!("parse: term");
             self.current += 1;
             let operator = self.previous_token();
             let right = self.factor();
@@ -104,10 +102,10 @@ impl Parser<'_> {
     }
 
     fn factor(&mut self) -> Expr {
-        println!("parse: factor");
         let mut expr = self.unary();
         let factor_fields = [TokenType::Slash, TokenType::Star];
         while let Some(_value) = self.tokens.next_if(|x| factor_fields.contains(&x.token_type)) {
+            println!("parse: factor");
             self.current += 1;
             let operator = self.previous_token();
             let right = self.unary();
@@ -120,6 +118,7 @@ impl Parser<'_> {
         // todo: fix this, currently allows multiple leading negatives (unless caught at lexer?)
         let unary_fields = [TokenType::Bang, TokenType::Minus];
         if let Some(_value) = self.tokens.next_if(|x| unary_fields.contains(&x.token_type)) {
+            println!("parse: unary");
             self.current += 1;
             let operator = self.previous_token();
             let right = self.unary();
@@ -130,28 +129,32 @@ impl Parser<'_> {
 
 
     fn primary(&mut self) -> Expr {
-        println!("parse: primary");
         // todo: resolve return value from this, implement indexing for Vec<Token>
         if let Some(_value) = self.tokens.next_if(|x| x.token_type == TokenType::False) {
+            println!("parse: bool");
             self.current += 1;
             return Expr::Boolean(false);
         }
         if let Some(_value) = self.tokens.next_if(|x| x.token_type == TokenType::True) {
+            println!("parse: bool");
             self.current += 1;
             return Expr::Boolean(true);
         }
         if let Some(_value) = self.tokens.next_if(|x| x.token_type == TokenType::Nil) {
+            println!("parse: nil");
             self.current += 1;
             return Expr::Nil;
         }
 
         if let Some(_value) = self.tokens.next_if(|x| x.token_type == TokenType::String) {
+            println!("parse: nil");
             self.current += 1;
             let token = self.previous_token();
             return Expr::String(token.lexeme);
         }
 
         if let Some(_value) = self.tokens.next_if(|x| x.token_type == TokenType::Number) {
+            println!("parse: number");
             self.current += 1;
             let token = self.previous_token();
             let number = token.lexeme.parse::<f32>().unwrap();
@@ -159,6 +162,7 @@ impl Parser<'_> {
         }
 
         if let Some(_value) = self.tokens.next_if(|x| x.token_type == TokenType::LeftParen) {
+            println!("parse: grouping");
             self.current += 1;
             let expr = self.expression();
             while let Some(value) = self.tokens.peek() {
